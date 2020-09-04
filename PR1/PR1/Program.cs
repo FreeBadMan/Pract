a8332db171489bdff39599f6ddc45f3e7318bdca
freebadman <freeman1813@gmail.com>
2020-09-04 11:13:22 +0300

    1.2

--- PR1/PR1/Program.cs
+++ PR1/PR1/Program.cs
@@ -6,23 +6,25 @@ namespace pract1
   {
     struct Person
     {
+      public enum Genders : int { Male, Female };
       public string firstName;
       public string lastName;
       public int age;
-      public Person(string _firstName, string _lastName, int _age)
+      public Genders gender;
+      public Person(string _firstName, string _lastName, int _age, Genders _gender)
       {
-        firstName = _firstName;
-        lastName = _lastName; age = _age;
-      }
-      public override string ToString()
-      {
-        return firstName + " " + lastName + ", age " + age;
-      }
+        firstName = _firstName; lastName = _lastName; age = _age; gender = _gender;
+        
     }
-    static void Main(string[] args)
+    public override string ToString()
     {
-      Person p = new Person("Tony", "Allen", 32); Console.WriteLine(p);
-
+      return firstName + " " + lastName + " (" + gender + "), age " + age;
     }
   }
+  static void Main(string[] args)
+  {
+    Person p = new Person("Tony", "Allen", 32, Person.Genders.Male);
+    Console.WriteLine(p.ToString());
+  }
+}
 }
